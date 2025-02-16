a6902e49ef4c682554d6ad2db86f557a11bd0f16
hbtpoprock <gunnuu@hotmail.com>
2025-02-16 15:06:42 +0700

    add async validateUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (user && bcrypt.compare(user.password, pass)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
