a71e2cc7b80070e36d32cf76fd8cba2ea7d2f44a
hbtpoprock <gunnuu@hotmail.com>
2025-02-16 18:37:01 +0700

    add UserPayload and JwtPayload interface

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -2,3 +2,15 @@ export const jwtConstants = {
   secret:
     'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
 };
+
+export interface UserPayload {
+  _id: string;
+  username: string;
+  createdAt: Date;
+  __v: number;
+}
+
+export interface JwtPayload {
+  username: string;
+  sub: string;
+}
