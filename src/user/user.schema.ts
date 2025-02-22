a87a0c5cf241eb3d7892cc3e65e8c3c9d3fa7583
hbtpoprock <gunnuu@hotmail.com>
2025-02-22 17:19:14 +0700

    implement updateAt field

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
   @Prop({ required: true })
   username: string;
@@ -16,9 +16,6 @@ export class User {
 
   @Prop({ required: true })
   age: number;
-
-  @Prop({ default: Date.now })
-  createdAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
