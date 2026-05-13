d57e44972b90312f86e1d44c6d7b115d2eb715ce
hbtpoprock <gunnuu@hotmail.com>
2026-05-13 17:17:22 +0700

    fix No open ports detected error when trying to deploy on Render

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
